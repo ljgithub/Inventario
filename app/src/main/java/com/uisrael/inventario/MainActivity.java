c341a9098cce1ca44e17670d2d7571218a8b4342
Xavier Morales <jjmm7@live.com>
2019-12-24 11:30:25 -0500

    Se intenta conectar con la base de postgres

--- app/src/main/java/com/uisrael/inventario/MainActivity.java
+++ app/src/main/java/com/uisrael/inventario/MainActivity.java
@@ -2,6 +2,7 @@ package com.uisrael.inventario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,7 @@ import java.sql.Connection;
 import  java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     String baseDatos = "kcatkipg";
 
@@ -61,13 +62,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean guardarProducto(View v){
-        boolean res = conectarAMySql();
+        //boolean res = conectarAMySql();
 
 
-        return res;
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                String ip = "rajje.db.elephantsql.com";
+                String puerto = "3306";
+                String user= "kcatkipg";
+                String password = "rBJJrCOygZBKni0U74j6B5w9Xvbhr9pv";
+
+                boolean estadoConeccion = false;
+                String driver = "org.postgresql.Driver";
+
+                String urlMySQL = "jdbc:postgresql://" + ip + ":" + puerto + "/";
+
+                Connection conexionMySql = null;
+
+                try {
+                    Class.forName(driver).newInstance();
+                    conexionMySql = DriverManager.getConnection(urlMySQL + baseDatos  ,  user, password);
+
+                    if (!conexionMySql.isClosed()){
+                        estadoConeccion = true;
+                        Toast.makeText(getApplicationContext(), "Conexion Establecida", Toast.LENGTH_LONG).show();
+                    }
+                }catch (Exception ex){
+                    Toast.makeText(getApplicationContext(), "No se pudo establecer la conexión!!!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        return true;
     }
 
-    public boolean conectarAMySql(){
+    public boolean conectarAMySql() {
 
         String ip = "rajje.db.elephantsql.com";
         String puerto = "3306";
